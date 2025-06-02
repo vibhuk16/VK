0af60adbacf28559e791694c43a6c9121b147dac
Vibhu Kumar <vibhukumar@c02wp22vhtdd.fritz.box>
2025-06-02 18:48:11 +0200

    Configure for deployment to /VK/ subdirectory

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/VK">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/analytics" element={<Analytics />} />
